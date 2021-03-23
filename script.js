e91a7c6b23de8c5601a14d98f4f1a349e0c54223
rafaeldellaquila <rafadellaquila@gmail.com>
2021-03-23 02:10:59 -0300

    :ambulance: ajustando conversão correta do dolar para o real

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getValue() {
-  var myValue = document.getElementById("ValorReal").value;
+  var myValue = document.getElementById("dolarValue").value;
   var dolarConvert = parseFloat(myValue * 5.51).toFixed(2);
-  document.getElementById("dolarValue").innerHTML = dolarConvert;
+  document.getElementById("realValue").innerHTML = dolarConvert;
 }
