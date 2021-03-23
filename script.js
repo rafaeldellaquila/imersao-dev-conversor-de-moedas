bd468ab4c800f310d745ad3d27df11fd26c42b01
rafaeldellaquila <rafadellaquila@gmail.com>
2021-03-23 13:28:41 -0300

    :sparkles: add eur convert and light convert

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function getValue() {
-  var myValue = document.getElementById("dolarValue").value;
-  var dolarConvert = parseFloat(myValue * 5.51).toFixed(2);
-  document.getElementById("realValue").innerHTML = dolarConvert;
+function getValueDolar() {
+  var myValue = document.getElementById("cathValue").value;
+  var valueConvert = parseFloat(myValue * 5.51).toFixed(2);
+  document.getElementById("convertedValue").innerHTML = valueConvert;
+}
+
+function getValueEuro() {
+  var myValue = document.getElementById("cathValueEuro").value;
+  var valueConvert = parseFloat(myValue * 6.51).toFixed(2);
+  document.getElementById("convertedValueEuro").innerHTML = valueConvert;
+}
+
+function getValueLight() {
+  var myValue = document.getElementById("cathValueKm").value;
+  var valueConvert = parseFloat(myValue * 9460536068); //.toFixed(2);
+  document.getElementById("convertedValueLight").innerHTML = valueConvert;
 }
