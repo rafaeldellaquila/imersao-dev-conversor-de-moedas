128f6e2dec1c46fbcfb30553c088dd3b47770953
rafaeldellaquila <rafadellaquila@gmail.com>
2021-03-23 00:49:49 -0300

    :lipstick: parte de estrutura e  styles  pronta para começar a inserir a lógica

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert("Tá funcionando?");
+var ValorEmDolarTexto = prompt(
+  "Qual o valor em dolar que você quer converter?"
+);
+
+var ValorFixed = parseFloat(ValorEmDolarTexto * 5.51).toFixed(2);
+
+alert(ValorFixed);
